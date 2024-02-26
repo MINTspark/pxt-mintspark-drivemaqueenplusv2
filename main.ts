e03656dbbb521ba3e27f5aa197f121be95d4445a
mpoelzl <mpoelzl@redeaglesolutions.com>
2024-02-26 08:43:11 +0100

    Start

--- main.ts
+++ main.ts
@@ -1,3 +1,27 @@
-basic.forever(function () {
-	
-})
+/**
+ * Custom graphic block
+ */
+//% weight=100 color=#0fbc11 icon="\uf067" block="Easy maqueenPlusV2"
+namespace EasyMaqueenPlusV2 {
+    let steeringCorrection = 0;
+
+    //% block="set steering correction %correction (positive number turns more to the right)"
+    //% weight=98
+    export function setSteeringCorrection(correction: number): void {
+        steeringCorrection = correction;
+    }
+
+    //% block="drive %edir speed %speed"
+    //% speed.min=0 speed.max=255
+    //% weight=99
+    export function drive(direction: maqueenPlusV2.MyEnumDir, speed: number): void {
+        maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.LeftMotor, direction, speed + steeringCorrection);
+        maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.RightMotor, direction, speed);
+    }
+
+    //% block="stop"
+    //% weight=98
+    export function controlMotorStop(): void {
+        maqueenPlusV2.controlMotorStop(maqueenPlusV2.MyEnumMotor.AllMotor);
+    }
+}
